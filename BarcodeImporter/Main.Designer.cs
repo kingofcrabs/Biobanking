7da4f2878b02a39c6570c1c221498e4c7e1d0c5d
Rex <rex.xie@qq.com>
2016-11-22 16:07:12 +0800

    import barcode

---
+++ BarcodeImporter/Main.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace BarcodeImporter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalCnt = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.richTextInfo = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(492, 415);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "病人信息：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(396, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "版本号：";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(455, 9);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(0, 12);
+            this.lblVersion.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 442);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "病人数量：";
+            // 
+            // lblTotalCnt
+            // 
+            this.lblTotalCnt.AutoSize = true;
+            this.lblTotalCnt.Location = new System.Drawing.Point(461, 442);
+            this.lblTotalCnt.Name = "lblTotalCnt";
+            this.lblTotalCnt.Size = new System.Drawing.Size(0, 12);
+            this.lblTotalCnt.TabIndex = 19;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Enabled = false;
+            this.btnOk.Location = new System.Drawing.Point(431, 547);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 20;
+            this.btnOk.Text = "确认";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(350, 547);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 23);
+            this.btnAbort.TabIndex = 21;
+            this.btnAbort.Text = "放弃";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(269, 547);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 22;
+            this.btnImport.Text = "导入";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // richTextInfo
+            // 
+            this.richTextInfo.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextInfo.Location = new System.Drawing.Point(14, 457);
+            this.richTextInfo.Name = "richTextInfo";
+            this.richTextInfo.ReadOnly = true;
+            this.richTextInfo.Size = new System.Drawing.Size(492, 86);
+            this.richTextInfo.TabIndex = 24;
+            this.richTextInfo.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(11, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "错误信息：";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 582);
+            this.Controls.Add(this.richTextInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblTotalCnt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Main";
+            this.Text = "条码导入";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotalCnt;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.RichTextBox richTextInfo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BarcodeImporter/Main.cs
@@ -0,0 +1,165 @@
+﻿using Settings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BarcodeImporter
+{
+    public partial class Main : Form
+    {
+
+        List<PatientInfo> patientInfos = new List<PatientInfo>();
+        public Main()
+        {
+            InitializeComponent();
+            WriteResult(false);
+
+        }
+
+
+        private void InitDataGridView(int totalCnt)
+        {
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.EnableHeadersVisualStyles = false;
+            dataGridView.Columns.Clear();
+            List<string> strs = new List<string>();
+
+            int gridCnt = (totalCnt + 15) / 16;
+
+            int srcStartGrid = 1;
+            for (int i = 0; i < gridCnt; i++)
+            {
+                DataGridViewTextBoxColumn column = new DataGridViewTextBoxColumn();
+                column.HeaderText = string.Format("条{0}", srcStartGrid + i);
+                column.HeaderCell.Style.BackColor = Color.LightSeaGreen;
+                dataGridView.Columns.Add(column);
+                dataGridView.Columns[i].SortMode = DataGridViewColumnSortMode.Programmatic;
+            }
+            dataGridView.RowHeadersWidth = 80;
+            for (int i = 0; i < 16; i++)
+            {
+                dataGridView.Rows.Add(strs.ToArray());
+                dataGridView.Rows[i].HeaderCell.Value = string.Format("行{0}", i + 1);
+            }
+        }
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            WriteResult(true);
+            WriteBarcodes();
+            this.Close();
+        }
+        private void WriteBarcodes()
+        {
+            string exePath = Utility.GetExeFolder() + "Biobanking.exe";
+            Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+            string file = config.AppSettings.Settings["SrcBarcodeFile"].Value;
+            File.WriteAllLines(file, patientInfos.Select(x => x.id));
+        }
+        private void WriteResult(bool bok)
+        {
+            string folder = Utility.GetOutputFolder();
+            string resultFile = folder + "barcodeResult.txt";
+            File.WriteAllText(resultFile, bok.ToString());
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ImportImpl();
+            }
+            catch(Exception ex)
+            {
+                AddErrorInfo(ex.Message + ex.StackTrace);
+            }
+            
+           
+        }
+
+        private void ImportImpl()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "请选择文件";
+            dialog.RestoreDirectory = true;
+            dialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+            string selectfile = dialog.FileName;
+            List<string> strs = File.ReadAllLines(selectfile,Encoding.Default).ToList();
+            strs = strs.Skip(1).ToList();
+            foreach (string s in strs)
+            {
+                if (s == "")
+                    break;
+                char chSplit = ',';
+                if (!s.Contains(','))
+                {
+                    if (s.Contains('\t'))
+                        chSplit = '\t';
+                }
+
+                string[] tempStrs = s.Split(chSplit);
+                if (tempStrs[0] == "")
+                    break;
+                patientInfos.Add(new PatientInfo(tempStrs[0], tempStrs[1]));
+            }
+
+            InitDataGridView(patientInfos.Count);
+            int maxAllowed = int.Parse(ConfigurationManager.AppSettings["maxSampleCnt"]);
+            if(patientInfos.Count > maxAllowed)
+                throw new Exception(string.Format("一共有{0}个样品，超过最大允许数{1}",patientInfos.Count,maxAllowed));
+
+            for (int i = 0; i < patientInfos.Count; i++)
+            {
+                int tubeID = i + 1;
+                var gridID = (tubeID + 15) / 16;
+                int rowIndex = tubeID - (gridID - 1) * 16 - 1;
+                UpdateGridCell(gridID, rowIndex, patientInfos[i]);
+            }
+            lblTotalCnt.Text = patientInfos.Count.ToString();
+            btnOk.Enabled = true;
+            AddInfo("导入成功，请校验！");
+        }
+
+
+        private void AddErrorInfo(string txt)
+        {
+            richTextInfo.SelectionColor = Color.Red;
+            richTextInfo.AppendText(txt + "\r\n");
+        }
+
+        private void AddInfo(string txt)
+        {
+            richTextInfo.SelectionColor = Color.Green;
+            richTextInfo.AppendText(txt + "\r\n");
+        }
+
+        private void UpdateGridCell(int gridID, int rowIndex,PatientInfo patientInfo)
+        {
+            dataGridView.Rows[rowIndex].Cells[gridID - 1].Value = patientInfo.id + "_" + patientInfo.name;
+        }
+    }
+
+    public struct PatientInfo
+    {
+        public string id;
+        public string name;
+        public PatientInfo(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
