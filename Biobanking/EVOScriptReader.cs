3ab5fd4e1ad59ca5ae8ba8d97fa924dfcfa43cbe
Rex <rex.xie@qq.com>
2017-09-05 14:54:44 +0800

    add script

---
+++ Biobanking/EVOScriptReader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biobanking
+{
+
+    public enum CarrierType
+    {
+        SampleTube16Pos = 0,
+        Waste,
+        Eppendorf16Pos,
+        MicroPlate96Well,
+        Other,  
+    };
+
+    public class EVOScriptReader
+    {
+        public string sScriptFile = ConfigurationManager.AppSettings["ScriptFile"];
+
+        public Dictionary<int, CarrierType> Read()
+        {
+            if (!File.Exists(sScriptFile))
+                throw new Exception("Cannot find script file at:" + sScriptFile);
+            List<string> sGridDescriptions = new List<string>();
+            List<string> sContents = File.ReadAllLines(sScriptFile).ToList();
+            sGridDescriptions = sContents.Where(s => s.Contains("998")).ToList();
+            return ParseAll(sGridDescriptions);
+        }
+
+        private Dictionary<int, CarrierType> ParseAll(List<string> sGridDescriptions)
+        {
+            Dictionary<int, CarrierType> label_basicDef_dict
+                = new Dictionary<int, CarrierType>();
+
+            int grid = 0;
+            int maxGrid = Math.Min(sGridDescriptions.Count - 1, 69);
+            for (int i = 0; i < maxGrid; i++)
+            {
+                string s = sGridDescriptions[i];
+                if (s == "998;0;")
+                    grid++;
+                if (s == "998;0;" || s == "998;1;" || s == "998;4;0;System;")
+                    continue;
+
+
+                label_basicDef_dict.Add(grid, ParseType(s));
+                grid++;
+                i++;
+            }
+            return label_basicDef_dict;
+        }
+
+        private CarrierType ParseType(string sLabels)
+        {
+            if (sLabels.Contains("DiTi Waste"))
+                return CarrierType.Waste;
+            if (sLabels.Contains("Tube Eppendorf 16 Pos"))
+                return CarrierType.Eppendorf16Pos;
+            if (sLabels.Contains("Tube 16*"))
+                return CarrierType.SampleTube16Pos;
+            if (sLabels.Contains("96 Well"))
+                return CarrierType.MicroPlate96Well;
+            else
+                return CarrierType.Other;
+        }
+    }
+    
+
+ 
+}
---
+++ Biobanking/LayoutChecker.cs
@@ -0,0 +1,95 @@
+﻿using Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biobanking
+{
+    class LayoutChecker
+    {
+        LabwareSettings labwareSettings;
+        PipettingSettings pipettingSettings;
+        int sampleCnt;
+        public LayoutChecker(LabwareSettings labwareSettings, PipettingSettings pipettingSettings,int sampleCnt)
+        {
+            this.labwareSettings = labwareSettings;
+            this.pipettingSettings = pipettingSettings;
+            this.sampleCnt = sampleCnt;
+        }
+
+        public void Check()
+        {
+            EVOScriptReader scriptReader = new EVOScriptReader();
+            var dict = scriptReader.Read();
+            //check start grid is 16 pos sample tubes
+            int startGrid = labwareSettings.sourceLabwareStartGrid;
+            int nGridCnt = labwareSettings.sourceLabwareGrids;
+            for(int i = 0; i< nGridCnt; i++)
+            {
+                int grid = i + startGrid;
+                if (!dict.ContainsKey(grid))
+                    throw new Exception(string.Format("Source grid:{0} doesnot exist!", grid));
+                if( dict[grid] != CarrierType.SampleTube16Pos)
+                    throw new Exception(string.Format("Source grid:{0}'s labware is NOT 16 position sample tube!", grid));
+            }
+
+
+            //check dest grid is MicroPlate
+            startGrid = labwareSettings.dstLabwareStartGrid;
+            nGridCnt = labwareSettings.dstCarrierCnt;
+            int gridsPerCarrier = labwareSettings.gridsPerCarrier;
+            for(int i = 0; i< nGridCnt; i++)
+            {
+                int grid = startGrid + gridsPerCarrier * i;
+                if (!dict.ContainsKey(grid))
+                    throw new Exception(string.Format("Dest grid:{0} doesnot exist!", grid));
+                if (dict[grid] != CarrierType.MicroPlate96Well)
+                    throw new Exception(string.Format("Dest grid:{0}'s labware is NOT 96 microPlate!", grid));
+            }
+
+            nGridCnt = (sampleCnt + 15) / 16;
+            //check transfer tubes
+            if(pipettingSettings.needTransfer)
+            {
+                startGrid = labwareSettings.transferGrid;
+                for (int i = 0; i < nGridCnt; i++)
+                {
+                    int grid = i + startGrid;
+                    if (!dict.ContainsKey(grid))
+                    {
+                        throw new Exception(string.Format("Transfer grid:{0} doesnot exist!", grid));
+                    }
+                    if (dict[grid] != CarrierType.Eppendorf16Pos)
+                    {
+                        throw new Exception(string.Format("Transfer grid:{0}'s labware is NOT 16 position eppendorf sample tube!", grid));
+                    }
+                }
+              
+            }
+
+            if (labwareSettings.buffyTransferGrid < labwareSettings.transferGrid)
+                throw new Exception("Buffy transfer grid cannot < plasma transfer grid!");
+
+            //check 
+            if(pipettingSettings.dstbuffySlice == 2)
+            {
+                startGrid = labwareSettings.buffyTransferGrid;
+                for (int i = 0; i < nGridCnt; i++)
+                {
+                    int grid = i + startGrid;
+                    if (!dict.ContainsKey(grid))
+                    {
+                        throw new Exception(string.Format("Transfer grid for buffy:{0} doesnot exist!", grid));
+                    }
+                    if (dict[grid] != CarrierType.Eppendorf16Pos)
+                    {
+                        throw new Exception(string.Format("Transfer grid for buffy:{0}'s labware is NOT 16 position eppendorf sample tube!", grid));
+                    }
+                }
+            }
+
+        }
+    }
+}
